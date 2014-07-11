ac73c1894fd82ed8e86726e499036fe662656cda
gulsumygt <glsm.ygt@hotmail.com>
2014-07-11 16:37:18 +0300

    makale örneği düzenlendi

--- MVCprojesi/Global.asax.cs
+++ MVCprojesi/Global.asax.cs
@@ -20,36 +20,23 @@ namespace MVCprojesi
                 //Bu metod, eğer veritabanımız oluşturulmamış ise, oluşturulmasını sağlıyor.
                 db.Database.CreateIfNotExists();
                 //Buraya örnek olması açısından 6 adet makale ekleyelim.
-                /*
-                 * makaleleri her çalıştırdığımızda eklememei için yorum satırı haline getirdik.
-                //Makaleden bir nesne oluşturuyoruz.
-                Makale makale = new Makale();
-                makale.Baslik = "Makale Başlığı 1";
-                makale.Icerik = "Makale İçeriği 1";
-                //Tarih olarak, şu anki tarihi veriyoruz.
-                makale.Tarih = DateTime.Now;
-
-                //Aynı işlemi diğer makaleler için de tekrarlıyoruz.
-                //Bu sefer daha kısa tanımlama yöntemini kullandık.
-                Makale makale2 = new Makale() { Baslik = "Makale Başlığı 2", Icerik = "Makale İçeriği 2", Tarih = DateTime.Now };
-                Makale makale3 = new Makale() { Baslik = "Makale Başlığı 3", Icerik = "Makale İçeriği 3", Tarih = DateTime.Now };
-                Makale makale4 = new Makale() { Baslik = "Makale Başlığı 4", Icerik = "Makale İçeriği 4", Tarih = DateTime.Now };
-                Makale makale5 = new Makale() { Baslik = "Makale Başlığı 5", Icerik = "Makale İçeriği 5", Tarih = DateTime.Now };
-                Makale makale6 = new Makale() { Baslik = "Makale Başlığı 6", Icerik = "Makale İçeriği 6", Tarih = DateTime.Now };
-
-                //Veritabanında ki Makalelere eklemek için ekleme komutu veriyoruz.
-                //SaveChanges() komutu gelene kadar veritabanına kaydedilmeyecek.
-                db.Makales.Add(makale);
-                db.Makales.Add(makale2);
-                db.Makales.Add(makale3);
-                db.Makales.Add(makale4);
-                db.Makales.Add(makale5);
-                db.Makales.Add(makale6);
-
-                //Son olarak ta yaptığımız eklemelerin, veritabanına yansıtılmasını
-                //sağlamak için kaydet komutu veriyoruz.
+                
+                /* makaleleri her çalıştırdığımızda eklememei için yorum satırı haline getirdik.
+                //Makaleden nesne oluşturuyoruz.
+                  for(int i=1;i<=6;i++){
+                  Makale makale = new Makale();
+                  makale.Baslik = "Makale Başlığı";
+                  makale.Icerik ="Makale İçeriği";
+                  makale.Tarih = DateTime.Now;
+                      //veri tabanında makaleleri eklemek için ekleme komutu veriyoruz
+                      db.Makales.Add(makale);
+                  }
+                  //SaveChanges() komutu gelene kadar veritabanına kaydedilmeyecek.
+                  //Son olarak ta yaptığımız eklemelerin, veritabanına yansıtılmasını
+                  //sağlamak için kaydet komutu veriyoruz.
                 db.SaveChanges();
                  */
+               
             }
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
